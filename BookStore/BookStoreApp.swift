430c89750d8a8daf699798f5bd684e811ddf0949
parisollie <pjff18@gmail.com>
2024-07-27 19:34:50 -0600

    Initial Commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by Paul Jaime Felix Flores on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
